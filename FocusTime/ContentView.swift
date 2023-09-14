22347c26168e1a8ee179e4636fb1c13d799ca445
José Tone <developer.tony17@gmail.com>
2023-09-14 11:29:59 +0100

    Initial Commit

---
+++ FocusTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FocusTime
+//
+//  Created by José Tony on 14/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FocusTime/FocusTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimeApp.swift
+//  FocusTime
+//
+//  Created by José Tony on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
